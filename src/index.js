a49fefde28d4dfe2bfa43924bffdbc2d068bcaae
bst003 <bstanner33@yahoo.com>
2023-04-26 23:58:38 -0400

    Continue adding notes for game

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ ROUTING
     - ROUTES
         - Start 
         - Game
-        - Scoreboard/Play Again
+        - Score/Play Again (Will have option to submit score)
+            - DOES THIS NEED TO BE ITS OWN ROUTE?
+        - Leaderboard
     
 ORGANIZATION
     - HOW TO BREAK DOWN DIRECTORIES
@@ -25,14 +27,32 @@ ORGANIZATION
                 - Items such as buttons, content containers.
             - Folder for each compontent based on context 
                 - IE: folder for start screen components, folder for game components, 
-                  folder for scorebopard components
+                  folder for scoreboard components, folder for timer components
         - Views
             - The main template files
         - Parts
             - Stuff like Header and Footer?
 
-FUNCTIONALITY
-    1. 
+GAMEFLOW
+    1. User lands on start page that has two options, Start Game and Leaderboard
+        - Start Game leads to Game View
+        - Leaderboard leads to Leaderboard view
+    2. User lands on the Game page
+        - Once the view loads the timer will start to count down
+            - Where to store timer ingo?
+        - User will be able to click on picture element and will track clicks with Mouse 
+          Event (clientX/Y) + 100 or so px in each direction
+          https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/clientX
+        - When user clicks a dropdown of options will be shown with the name of each character.
+            - The dropdown will remove items already found
+        - Will then verify if those coordinates match what is stored in Firebase for the 
+          character selected.
+            - If correct a pin will be placed in that area of the image and an alert will appear in green
+            - If incorrect an alert will appear in red
+        - Once the user finds all of the characters they will get have a way to 
+          enter their name and submit their score. This will be a popup or a whole new view.
+            - This popup/view will also have buttons to restart and view leaderboard
+    3. 
 
 */
 
