c1c7771dae76ada64a8518fcff3801e6794d1b1e
bst003 <bstanner33@yahoo.com>
2023-05-08 22:32:55 -0400

    Updat styling for LevelSelectCTA

--- src/components/home/LevelSelectCTA.js
+++ src/components/home/LevelSelectCTA.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import "./LevelSelectCTA.scss";
+
 const LevelSelectCTA = (props) => {
     const { codename, label } = props;
 
@@ -10,7 +12,7 @@ const LevelSelectCTA = (props) => {
     return (
         <button className="level-select-cta" type="button" onClick={startGame}>
             <img src={require(`../../assets/img/${codename}-thumb.jpg`)} alt={label + " level"} />
-            {label}
+            <span>{label}</span>
         </button>
     );
 };
