0998b67772e8dbfdcbe2f48fa764f68527b26092
bst003 <bstanner33@yahoo.com>
2023-05-13 22:53:29 -0400

    Begin Finder calculation functions

--- src/components/game/Finder.js
+++ src/components/game/Finder.js
@@ -5,8 +5,32 @@ import "./Finder.scss";
 const Finder = (props) => {
     const { codename, label } = props;
 
+    const getCoords = (e) => {
+        console.log(e);
+        const coordX = e.nativeEvent.offsetX;
+        const coordY = e.nativeEvent.offsetY;
+        console.log(`coord X: ${coordX}`);
+        console.log(`coord Y: ${coordY}`);
+
+        return [coordX, coordY];
+    };
+
+    const getFinderDimensions = (e) => {
+        const finderWidth = e.target.width;
+        const finderHeight = e.target.height;
+        console.log(`width: ${finderWidth}`);
+        console.log(`height: ${finderHeight}`);
+
+        return [finderWidth, finderHeight];
+    };
+
+    const triggerClick = (e) => {
+        const clickCoords = getCoords(e);
+        const finderDimensions = getFinderDimensions(e);
+    };
+
     return (
-        <figure className="fg-finder">
+        <figure className="fg-finder" onClick={triggerClick}>
             <img src={require(`../../assets/img/${codename}.png`)} alt={label + " level"} />
         </figure>
     );
