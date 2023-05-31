d531cf30849bd492e34e554140b791bd5e0ab9f1
bst003 <bstanner33@yahoo.com>
2023-05-30 22:44:52 -0400

    Begin adding content to StartOverlay

--- src/components/game/StartOverlay.js
+++ src/components/game/StartOverlay.js
@@ -12,7 +12,11 @@ const StartOverlay = (props) => {
     return (
         <div className="start-overlay">
             <div className="start-overlay__inner">
-                the game
+                <h2>Let's Play!</h2>
+                <p>
+                    Find all three characters in the nav to the left in order to win the game.
+                    Compete to earn the top on spot on the leaderboard for each level. Good luck!
+                </p>
                 <button type="button" onClick={startGame}>
                     Start game
                 </button>
