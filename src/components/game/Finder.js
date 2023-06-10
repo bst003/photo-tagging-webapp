dd13044fb61173f5a494c2b8d40fcd6b1749ecf8
bst003 <bstanner33@yahoo.com>
2023-06-10 00:06:31 -0400

    Modify pre-game text

--- src/components/game/Finder.js
+++ src/components/game/Finder.js
@@ -185,9 +185,10 @@ const Finder = (props) => {
                 <ContentBox>
                     <h2>Let's Play!</h2>
                     <p>
-                        Find all three characters in the nav to the left in order to win the game.
-                        Compete to earn the top on spot on the leaderboard for each level. Good
-                        luck!
+                        Find all three characters in the nav which can be found to the left on
+                        desktop and under the "Show Chars" button on mobile in order to win the
+                        game. Compete to earn the top on spot on the leaderboard for each level.
+                        Good luck!
                     </p>
                     <div className="btns-contain center">
                         <div className="btn">
