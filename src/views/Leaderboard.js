8164c4f7ebe802e156400df2b213f64926d10268
bst003 <bstanner33@yahoo.com>
2023-06-10 20:42:32 -0400

    Add paragraph text to Leaderboards view

--- src/views/Leaderboard.js
+++ src/views/Leaderboard.js
@@ -3,7 +3,13 @@ import LeaderboardGrid from "../components/leaderboards/LeaderboardGrid";
 const Leaderboard = () => {
     return (
         <div>
-            <h1 className="center-text">Leaderboards</h1>
+            <div className="intro-content">
+                <h1 className="center-text">Leaderboards</h1>
+                <p className="center-text">
+                    Below are the top ten times for each level, try your best to make the list!
+                </p>
+            </div>
+
             <LeaderboardGrid />
         </div>
     );
